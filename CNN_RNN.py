f6746f1a3617835af1c6a3308a8be384495bf0da
Chengrui Wu <wcrtju01@gmail.com>
2017-07-24 19:48:21 +0800

    Changed the CNN_RNN into con-rnn-decnn

--- CNN_RNN.py
+++ CNN_RNN.py
@@ -1,162 +1,183 @@
-import tensorflow as tf
 import numpy as np
-import cv2
+import tensorflow as tf
 import matplotlib.pyplot as plt
-import random
+import cv2
+import scipy.signal as signal
 
-LONGITUDE=24
-WIDTH=24
-PIXEL=LONGITUDE*WIDTH
-BATCHSIZE=10
-CONV_FRAME=3
-TIMESTEP=10
+n_inputs=3*3*32
+n_hidden_units = 512
+n_outputs=n_inputs
 
-n_inputs = 6*6*32
-n_hidden_units = 1024
-n_outputs = PIXEL
-n_steps = 10
-batch_size = 10
-lr = 0.001
-training_iters = 20000
-data_size = 5990
+def gray2binary(a):
+    for i in range(len(a)):
+        if a[i]>60:
+            a[i]=1
+        elif a[i]<=60:
+            a[i]=0
+    return a
 
-def get_batch(noise=1000):
-    ran = random.randint(600, data_size)
+def get_train_batch(noise=500):
+    #ran = np.random.randint(600,5800,size=100,dtype='int')
+    image = []
+    label = []
+    n_pic = np.random.randint(600,5800)
+    # print(n_pic)
+    for i in range(100):
+        frame_0 = cv2.imread('./cropedoriginalUS2/%d.jpg' % (n_pic + i), 0)
+        #frame_0 = add_noise(frame_0, n = noise)
+        frame_0 = cv2.resize(frame_0, (24, 24))
+        frame_0 = np.array(frame_0).reshape(-1)
+        frame_0 = frame_0 / 255.0
+        image.append(frame_0)
+        #print(np.shape(image))
+    for i in range(10):
+        frame_1 = cv2.imread('./cropedoriginalPixel2/%d.jpg' % (n_pic + 10*i +1), 0)
+        frame_1 = cv2.resize(frame_1, (24, 24))
+        frame_1 = np.array(frame_1).reshape(-1)
+        frame_1 = gray2binary(frame_1)
+        label.append(frame_1)
+    return np.array(image,dtype='float') , np.array(label,dtype='float')
+
+def get_test_batch(noise=500):
+    #ran = np.random.randint(5800,6000,size=100,dtype='int')
     #print(ran)
     image = []
     label = []
     label_0 = []
-    n_pic = ran
+    n_pic = np.random.randint(5800,6000)
     # print(n_pic)
-    for i in range(batch_size * n_steps):
-        frame_0 = cv2.imread('./easyPixelImage2/%d.jpg' % (n_pic+i), 0)
+    for i in range(100):
+        frame_0 = cv2.imread('./cropedoriginalUS2/%d.jpg' % (n_pic+i), 0)
         #frame_0 = add_noise(frame_0, n = noise)
-        frame_0 = cv2.resize(frame_0, (LONGITUDE, LONGITUDE))
+        frame_0 = cv2.resize(frame_0, (24, 24))
         frame_0 = np.array(frame_0).reshape(-1)
+        frame_0 = frame_0 / 255.0
         image.append(frame_0)
         #print(np.shape(image))
-    for i in range(batch_size):
-        frame_1 = cv2.imread('./easyPixelImage2/%d.jpg' % (n_pic + batch_size * (i+1) ), 0)
-        frame_1 = cv2.resize(frame_1, (LONGITUDE, LONGITUDE))
+    for i in range(10):
+        frame_1 = cv2.imread('./cropedoriginalPixel2/%d.jpg' % (n_pic + 10*i +1), 0)
+        frame_1 = cv2.resize(frame_1, (24, 24))
         frame_1 = np.array(frame_1).reshape(-1)
+        frame_1 = gray2binary(frame_1)
         label.append(frame_1)
-    for i in range(batch_size):
-        frame_2 = cv2.imread('./hardPixelImage/%d.jpg' % (n_pic + batch_size * (i+1) ), 0)
-        frame_2 = cv2.resize(frame_2, (LONGITUDE, LONGITUDE))
-        frame_2 = np.array(frame_2).reshape(-1)
-        label_0.append(frame_2)
-    return image , label , label_0
+    return np.array(image,dtype='float') , np.array(label,dtype='float')
 
-x = tf.placeholder(tf.float32, [n_steps, None, 24*24])
-y = tf.placeholder(tf.float32, [None, n_outputs])
+def input_norm(xs):
+    fc_mean, fc_var = tf.nn.moments(
+        xs,
+        axes=[0],
+    )
+    scale = tf.Variable(tf.ones([1]))
+    shift = tf.Variable(tf.zeros([1]))
+    epsilon = 0.001
+    # apply moving average for mean and var when train on batch
+    ema = tf.train.ExponentialMovingAverage(decay=0.5)
 
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+    def mean_var_with_update():
+        ema_apply_op = ema.apply([fc_mean, fc_var])
+        with tf.control_dependencies([ema_apply_op]):
+            return tf.identity(fc_mean), tf.identity(fc_var)
 
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-def max_pool_2x2(x):
-  return tf.nn.avg_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
-#CNN start
+    mean, var = mean_var_with_update()
+    xs = tf.nn.batch_normalization(xs, mean, var, shift, scale, epsilon)
+    return xs
 
-W_conv_1=[]
-b_conv_1=[]
-conv_1=[]
-pool_1=[]
-W_conv_2=[]
-b_conv_2=[]
-conv_2=[]
-pool_2=[]
-cnn_out=[]
-x_image=[]
-rnn_in=[]
-for i in range(10):
-    W_conv_1 = weight_variable([CONV_FRAME, CONV_FRAME, 1, 8])
-    b_conv_1 = bias_variable([8])
-    x_image = tf.reshape(x[i], [-1, LONGITUDE, WIDTH, 1])
-    conv_1 = tf.nn.relu(conv2d(x_image, W_conv_1) + b_conv_1)
-    pool_1 = max_pool_2x2(conv_1)
+def batch_norm(Wx_plus_b,out_size):
+    fc_mean, fc_var = tf.nn.moments(
+        Wx_plus_b,
+        axes=[0],  # the dimension you wanna normalize, here [0] for batch
+        # for image, you wanna do [0, 1, 2] for [batch, height, width] but not channel
+    )
+    scale = tf.Variable(tf.ones([out_size]))
+    shift = tf.Variable(tf.zeros([out_size]))
+    epsilon = 0.001
+    # apply moving average for mean and var when train on batch
+    ema = tf.train.ExponentialMovingAverage(decay=0.5)
+    def mean_var_with_update():
+        ema_apply_op = ema.apply([fc_mean, fc_var])
+        with tf.control_dependencies([ema_apply_op]):
+            return tf.identity(fc_mean), tf.identity(fc_var)
+    mean, var = mean_var_with_update()
+    Wx_plus_b = tf.nn.batch_normalization(Wx_plus_b, mean, var, shift, scale, epsilon)
+    return Wx_plus_b
 
-    W_conv_2 = weight_variable([CONV_FRAME, CONV_FRAME, 8, 32])
-    b_conv_2 = bias_variable([32])
-    conv_2 = tf.nn.relu(conv2d(pool_1, W_conv_2) + b_conv_2)
-    pool_2 = max_pool_2x2(conv_2)
-    cnn_out = tf.reshape(pool_2, [-1, 6*6*32])
-    rnn_in.append(cnn_out)
-rnn_in = tf.transpose(rnn_in, [1, 0, 2])
+inputs_ = tf.placeholder(tf.float32, [None, 24, 24, 1])
+targets_ = tf.placeholder(tf.float32, [None, 24, 24, 1])
 
-# RNN start
 weights = {
-    # (576, 1024)
     'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
-    # (1024, 576)
-    'out': tf.Variable(tf.random_normal([n_hidden_units, n_outputs]))
-}
+    'out': tf.Variable(tf.random_normal([n_hidden_units, n_outputs]))}
 biases = {
-    # (1024, )
     'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ])),
-    # (576, )
-    'out': tf.Variable(tf.constant(0.1, shape=[n_outputs, ]))
-}
-# transpose the inputs shape from
-# X ==> ( batchsize * 10 steps, 576 inputs)
-X_in = tf.reshape(rnn_in, [-1, n_inputs])
-# into hidden
-# X_in ==> ( batchsize * 10 steps, 1024 hidden)
-X_in = tf.matmul(X_in, weights['in']) + biases['in']
-# X_in ==> ( batchsize , 10 steps, 1024 hidden)
-X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
+    'out': tf.Variable(tf.constant(0.1, shape=[n_outputs, ]))}
 
+conv1 = tf.layers.conv2d(inputs_, 64, (3,3), padding='same', activation=tf.nn.relu)
+conv1 = tf.layers.max_pooling2d(conv1, (2,2), (2,2), padding='same')
+conv1 = batch_norm(conv1,64)
+conv2 = tf.layers.conv2d(conv1, 64, (3,3), padding='same', activation=tf.nn.relu)
+conv2 = tf.layers.max_pooling2d(conv2, (2,2), (2,2), padding='same')
+conv2 = batch_norm(conv2,64)
+conv3 = tf.layers.conv2d(conv2, 32, (3,3), padding='same', activation=tf.nn.relu)
+conv3 = tf.layers.max_pooling2d(conv3, (2,2), (2,2), padding='same')
+conv3 = batch_norm(conv3,32)
+
+conv3 = tf.reshape(conv3, [10,10,3*3*32])
 cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
-init_state = cell.zero_state(batch_size, dtype=tf.float32)
-outputs, final_state = tf.nn.dynamic_rnn(cell, X_in, initial_state=init_state, time_major=False)
-
+init_state = cell.zero_state(10, dtype=tf.float32)
+outputs, final_state = tf.nn.dynamic_rnn(cell, conv3, initial_state=init_state, time_major=False)
 outputs = tf.unstack(tf.transpose(outputs, [1, 0, 2]))
-pred = tf.matmul(outputs[-1], weights['out']) + biases['out']  # shape = (1024, 576)
-cost = tf.reduce_mean(tf.square(tf.reshape(pred,[-1]) - tf.reshape(y,[-1])))
+pred = tf.matmul(outputs[-1], weights['out']) + biases['out']
+pred = tf.reshape(pred,[10,3,3,32])
 
-train_op = tf.train.AdamOptimizer(lr).minimize(cost)
+conv4 = tf.image.resize_nearest_neighbor(pred, (6,6))
+conv4 = tf.layers.conv2d(conv4, 32, (3,3), padding='same', activation=tf.nn.relu)
+conv4 = batch_norm(conv4,32)
+conv5 = tf.image.resize_nearest_neighbor(conv4, (12,12))
+conv5 = tf.layers.conv2d(conv5, 64, (3,3), padding='same', activation=tf.nn.relu)
+conv5 = batch_norm(conv5,64)
+conv6 = tf.image.resize_nearest_neighbor(conv5, (24,24))
+conv6 = tf.layers.conv2d(conv6, 64, (3,3), padding='same', activation=tf.nn.relu)
+conv6 = batch_norm(conv6,64)
+logits_ = tf.layers.conv2d(conv6, 2, (3,3), padding='same', activation=None)
+outputs_ = tf.nn.softmax(logits_, dim= -1,name='outputs_')
+outputs_ = outputs_[:,:,:,0]
+outputs_ = tf.reshape(outputs_ , [-1,24,24,1])
+
+cost = tf.reduce_mean(tf.square(tf.reshape(targets_,[-1]) - tf.reshape(outputs_,[-1])))
+# loss = tf.nn.softmax_cross_entropy_with_logits(labels=targets_, logits=outputs_)
+# cost = tf.reduce_mean(loss)
+optimizer = tf.train.AdamOptimizer(0.0005).minimize(cost)
 
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
-    step = 0
-    while step * batch_size < training_iters:
-        batch_xs, batch_ys ,_ = get_batch(0)
-        batch_xs = np.reshape(batch_xs,[batch_size, n_steps, 24*24])
-        sess.run(train_op , feed_dict = { x: batch_xs ,y: batch_ys })
-        # plt.ion()
-        # plt.show()
-        if step % 20 == 0:
-            loss = sess.run(cost, feed_dict={x: batch_xs ,y: batch_ys })
-            print('step ',step,' cost = ',loss )
-            #loss_history.append(loss)
-            # plt.plot(loss_history, '-b')
-            # plt.draw()
-            # plt.pause(0.01)
-        # image_p = sess.run(pred[-1], feed_dict={x: batch_xs ,y: batch_ys,})
-        # image_p = tf.reshape(image_p, [LONGITUDE, WIDTH])
-        # image_p = np.array( image_p , dtype = int)
-        # cv2.imwrite('image_predict/' + str(step) + '.jpg', image_p)
-        step += 1
+    loss_history=[]
+    for i in range(10000):
+        batch, img = get_train_batch()
+        batch= np.reshape(batch,[-1, 24, 24, 1])
+        img= np.reshape(img,[-1, 24, 24, 1])
+        sess.run(optimizer, feed_dict={inputs_: batch, targets_: img})
+        if i % 10 == 0:
+            batch_cost = sess.run(cost, feed_dict={inputs_: batch, targets_: img})
+            loss_history.append(batch_cost)
+            print("Batch: {} ".format(i), "Training loss: {:.4f}".format(batch_cost))
     print("Optimization Finishes!")
 
-    batch_xs, batch_ys, ys_0 = get_batch(0)
-    batch_xs = np.reshape(batch_xs, [batch_size, n_steps, 24*24])
-    # batch_ys = np.reshape(batch_ys, [batch_size, n_steps, n_inputs])
-    image_p = sess.run(pred, feed_dict={x: batch_xs, y: batch_ys})
+    batch_xs, batch_ys = get_test_batch()
+    batch_xs = np.reshape(batch_xs,[-1, 24, 24, 1])
+    batch_ys = np.reshape(batch_ys, [-1, 24, 24, 1])
+    image_p = sess.run(outputs_, feed_dict={inputs_: batch_xs, targets_: batch_ys})
     # image_p = gray2binary(image_p)
+    plt.figure(0)
+    plt.plot(loss_history)  # , '-o')
+    plt.xlabel('train')
+    plt.ylabel('loss')
 
+    plt.figure(1)
     f, a = plt.subplots(3, 10, figsize=(10, 3))
     for i in range(10):
-        a[0][i].imshow(np.reshape(ys_0[i], (LONGITUDE, LONGITUDE)))
-        a[1][i].imshow(np.reshape(batch_ys[i], (LONGITUDE, LONGITUDE)))
-        a[2][i].imshow(np.reshape(image_p[i], (LONGITUDE, LONGITUDE)))
-        #a[3][i].imshow(np.reshape(gray2binary(image_p[i]), (LONGITUDE, LONGITUDE)))
+        #a[0][i].imshow(np.reshape(ys_0[i], (LONGITUDE, LONGITUDE)))
+        a[0][i].imshow(np.reshape(batch_ys[i], (24, 24)))
+        a[1][i].imshow(np.reshape(batch_xs[i], (24, 24)))
+        a[2][i].imshow(np.reshape(image_p[i], (24, 24)))
     plt.show()
-
